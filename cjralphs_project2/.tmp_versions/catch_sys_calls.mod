95d38e47060a5898f37634c53fe2a012f722bccf
calebralphs <cjralphs@wpi.edu>
2020-02-13 13:51:38 -0500

    made tail syslog

---
+++ cjralphs_project2/.tmp_versions/catch_sys_calls.mod
@@ -0,0 +1,2 @@
+/home/caleb/operating_systems/cjralphs_project2/catch_sys_calls.ko
+/home/caleb/operating_systems/cjralphs_project2/catch_sys_calls.o
--- cjralphs_project2/test_catch_sys_calls.c
+++ cjralphs_project2/test_catch_sys_calls.c
@@ -2,14 +2,14 @@
 #include <sys/syscall.h>
 #include <stdio.h>
 
-#define __NR_cs3013_syscall1 355
+#define __NR_cs3013_syscall1 377
 
 void main () {
-    // test sys_cs3013_syscall1
-    syscall(__NR_cs3013_syscall1);
-
     // test  sys_open, sys_close
     FILE *fp;
     fp = fopen("zoinks.txt", "r");
     fclose(fp);
+
+    // test sys_cs3013_syscall1
+    syscall(__NR_cs3013_syscall1);
 }
