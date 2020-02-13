df56d7d7d4bfc2ac7bf0d20a675b8a7dffc836df
Caleb Ralphs <cjralphs@wpi.edu>
2020-02-13 12:40:54 -0500

    added testing for cs3013_syscall1

--- cjralphs_project2/test_catch_sys_calls.c
+++ cjralphs_project2/test_catch_sys_calls.c
@@ -1,7 +1,13 @@
 #include <sys/syscall.h>
 #include <stdio.h>
 
+#define __NR_cs3013_syscall1 355
+
 void main () {
+    // test sys_cs3013_syscall1
+    syscall(__NR_cs3013_syscall1);
+
+    // test  sys_open, sys_close
     FILE *fp;
     fp = fopen("zoinks.txt", "r");
     fclose(fp);
