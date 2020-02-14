5d54b3cbfe13a8f15fe6841f3763609a4761aa1e
Caleb Ralphs <cjralphs@wpi.edu>
2020-02-13 21:07:37 -0500

    fix user side

--- cjralphs_project2/part2/procAncestry.c
+++ cjralphs_project2/part2/procAncestry.c
@@ -13,18 +13,14 @@ void main(int argc, char* argv[]) {
         exit(-1);
     }
     int i;
-    int pid = atoi(argv[1]);
+    unsigned short pid = (unsigned short)atoi(argv[1]);
     struct ancestry response;
     // test sys_cs3013_syscall1
-    syscall(__NR_cs3013_syscall2, pid, &response);
+    syscall(__NR_cs3013_syscall2, &pid, &response);
     for (i = 0; i < 10; i++) {
-        if (response.ancestors[i] > 0) {
-            printf("(User Space) Ancestor %d: %d\n", i, response.ancestors[i]);
-        }
+        printf("(User Space) Ancestor %d: %d\n", i, response.ancestors[i]);
     }
     for (i = 0; i < 100; i++) {
-        if (response.children[i] > 0) {
-            printf("(User Space) Child %d: %d\n", i, response.children[i]);
-        }
+        printf("(User Space) Child %d: %d\n", i, response.children[i]);
     }
 }
